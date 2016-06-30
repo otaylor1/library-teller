2be07aee0845d18339d218c7381f56c586405d88
Olivia Taylor <obt1@pitt.edu>
2016-06-30 10:43:18 -0400

    print media details and rental lenght done

---
+++ Library/DVD.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    class DVD : Media
+    {
+        public override int RentalLength
+        {
+            get
+            {
+                return 7;
+            }
+
+            //set method not going to do anything
+            set
+            {
+
+            }
+        }
+
+        public override void PrintMediaDetails()
+        {
+            Console.WriteLine("DVD {0} ({1})" , Title, Length);
+            Console.WriteLine("Rented on: {0}", RentedDate);
+            Console.WriteLine("Due on; {0}", ReturnDueDate());
+            
+            
+        }
+
+        public override DateTime ReturnDueDate()
+        {
+            throw new NotImplementedException();
+        }
+        
+    }
+}
